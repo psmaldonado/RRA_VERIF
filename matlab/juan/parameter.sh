39cbc47f628a587a60a759073e782852b500f938
Paula Maldonado <paula.maldonado@cima.fcen.uba.ar>
2019-12-03 14:38:22 -0300

    Delete matlab folder

--- matlab/juan/parameter.sh
+++
@@ -1,7 +0,0 @@
-
-
-matlab -nodisplay < parameter1.m > parameter1.log
-matlab -nodisplay < parameter2.m > parameter2.log
-matlab -nodisplay < parameter3.m > parameter3.log
-matlab -nodisplay < parameter4.m > parameter4.log
-
